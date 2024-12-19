c0ad4026b4ebd73cea6c349b6b1dc438d54b6245
Adam Brewer <adam@adambrewer.co.uk>
2024-12-19 21:29:57 +0000

    start slider

--- src/clients/mix-server-client/src/app/bottom-bar/audio-control/audio-progress-slider/audio-progress-slider.component.ts
+++ src/clients/mix-server-client/src/app/bottom-bar/audio-control/audio-progress-slider/audio-progress-slider.component.ts
@@ -1,9 +1,9 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {DurationDisplayComponent} from "../duration-display/duration-display.component";
-import {MatSliderDragEvent, MatSliderModule} from "@angular/material/slider";
+import {MatSlider, MatSliderDragEvent, MatSliderModule} from "@angular/material/slider";
 import {AudioPlayerService} from "../../../services/audio-player/audio-player.service";
 import {FormsModule} from "@angular/forms";
-import {AsyncPipe} from "@angular/common";
+import {AsyncPipe, NgForOf} from "@angular/common";
 import {Subject, takeUntil} from "rxjs";
 
 @Component({
@@ -13,17 +13,31 @@ import {Subject, takeUntil} from "rxjs";
     DurationDisplayComponent,
     MatSliderModule,
     FormsModule,
-    AsyncPipe
+    AsyncPipe,
+    NgForOf
   ],
   templateUrl: './audio-progress-slider.component.html',
   styleUrl: './audio-progress-slider.component.scss'
 })
-export class AudioProgressSliderComponent implements OnInit, OnDestroy {
+export class AudioProgressSliderComponent implements OnInit, AfterViewInit, OnDestroy {
   private _unsubscribe$ = new Subject();
   private _currentTime: number = 0;
 
   constructor(public audioPlayer: AudioPlayerService) {
   }
+
+  markers = [
+    {position: 0},
+    {position: 10},
+    {position: 30},
+    {position: 50},
+    {position: 70},
+    {position: 90},
+    {position: 100},
+  ]; // Section marker positions in percentage
+
+  @ViewChild('slider')
+  public slider?: ElementRef<MatSlider>;
 
   public ngOnInit(): void {
     // This is very important to avoid NG0100: Expression has changed after it was checked
@@ -33,6 +47,10 @@ export class AudioProgressSliderComponent implements OnInit, OnDestroy {
       .subscribe(currentTime => {
         this._currentTime = currentTime;
       });
+  }
+
+  public ngAfterViewInit(): void {
+    console.log(this.slider?.nativeElement);
   }
 
   public ngOnDestroy(): void {
@@ -50,5 +68,15 @@ export class AudioProgressSliderComponent implements OnInit, OnDestroy {
 
   public sliderDragEnded(event: MatSliderDragEvent) {
     this.audioPlayer.seek(event.value);
+  }
+
+  public getWidth(marker: { position: number }, index: number): string {
+    const nextMarker = index < this.markers.length - 1 ? this.markers[index + 1] : {position: 100};
+
+    // console.log(this.sectionMarker?.nativeElement?.clientWidth);
+
+    const width = nextMarker.position - marker.position;
+
+    return `${width}%`;
   }
 }
