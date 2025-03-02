0645450a829b2887c8eb8d9e353694014dc3a430
Adam Brewer <adam@adambrewer.co.uk>
2025-03-02 10:44:44 +0000

    move skip logic to server side

--- src/api/MixServer.Application/Sessions/Commands/SetNextSession/SetNextSessionCommand.cs
+++
@@ -1,8 +0,0 @@
-namespace MixServer.Application.Sessions.Commands.SetNextSession;
-
-public class SetNextSessionCommand
-{
-    public int Offset { get; set; }
-
-    public bool ResetSessionState { get; set; }
-}
